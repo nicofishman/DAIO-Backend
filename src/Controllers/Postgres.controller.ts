124b2c0577bea5e2695d0e2cd76ae11278d11b5b
nicofishman <74215078+nicofishman@users.noreply.github.com>
2022-05-16 10:39:47 -0300

    Conecta a postgres

--- src/Controllers/Postgres.controller.ts
+++ src/Controllers/Postgres.controller.ts
@@ -27,17 +27,17 @@ type User = {
 
 export const getUsers = async (_req: Request, res: Response): Promise<void> => {
     const client = new Client({
-        connectionString: process.env.POSTGRES_URL,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASS,
-        ssl: true,
+        connectionString: process.env.POSTGRES_URI,
+        user: process.env.PGUSER,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        ssl: { rejectUnauthorized: false }
     });
-    client.connect();
+    await client.connect();
     const query = 'SELECT * FROM users';
     client.query(query, (err: any, result: any) => {
         if (err) {
-            console.log(err);
+            console.log('err' + err);
             res.json(err);
         } else {
             res.json(result.rows);
@@ -49,8 +49,11 @@ export const getUsers = async (_req: Request, res: Response): Promise<void> => {
 export const addUser = async (req: Request, res: Response): Promise<void> => {
     const user: User = req.body;
     const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        connectionString: process.env.POSTGRES_URL,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_NAME,
+        password: process.env.POSTGRES_PASS,
+        ssl: false,
     });
     client.connect();
     const query = `SELECT * FROM users WHERE spotifyId = '${user.spotifyId}'`;
